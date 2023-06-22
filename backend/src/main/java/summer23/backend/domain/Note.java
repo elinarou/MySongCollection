2a7e8986ef6c5cb0497b3eada02aa4bcc2c129a5
elinarou <elina_rouvinen@outlook.com>
2023-06-22 15:38:40 +0300

    Classes

--- backend/src/main/java/summer23/backend/domain/Note.java
+++ backend/src/main/java/summer23/backend/domain/Note.java
@@ -1,5 +1,87 @@
 package summer23.backend.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long note_id;
+
+    private String note;
+
+    @ManyToOne
+    @JoinColumn(name="song_id")
+    private Song song;
+
+    @ManyToOne
+    @JoinColumn(name="instrument_id")
+    private Instrument instrument;
     
+    @ManyToOne
+    @JoinColumn(name="type_id")
+    private Type type;
+
+    public Note() {
+        super();
+    }
+
+    public Note(String note, Song song, Instrument instrument, Type type) {
+        this.note = note;
+        this.song = song;
+        this.instrument = instrument;
+        this.type = type;
+    }
+
+    public Long getNote_id() {
+        return note_id;
+    }
+
+    public void setNote_id(Long note_id) {
+        this.note_id = note_id;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public Song getSong() {
+        return song;
+    }
+
+    public void setSong(Song song) {
+        this.song = song;
+    }
+
+    public Instrument getInstrument() {
+        return instrument;
+    }
+
+    public void setInstrument(Instrument instrument) {
+        this.instrument = instrument;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Note [note_id=" + note_id + ", note=" + note + "]";
+    } 
+
 }
--- backend/src/main/java/summer23/backend/domain/Song.java
+++ backend/src/main/java/summer23/backend/domain/Song.java
@@ -1,5 +1,73 @@
 package summer23.backend.domain;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Song {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long song_id;
+
+    @ManyToOne
+    @JoinColumn(name="artist_id")
+    private Artist artist;
     
+    @ManyToOne
+    @JoinColumn(name="genre_id")
+    private Genre genre;
+
+    @JsonIgnore
+	@OneToMany(cascade = CascadeType.ALL, mappedBy ="song")
+	private List<Note> notes;
+
+    public Song() {
+        super();
+    }
+
+    public Song(Artist artist, Genre genre) {
+        this.artist = artist;
+        this.genre = genre;
+    }
+
+    public Long getSong_id() {
+        return song_id;
+    }
+
+    public void setSong_id(Long song_id) {
+        this.song_id = song_id;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public String toString() {
+        return "Song [song_id=" + song_id + "]";
+    }
+
 }
