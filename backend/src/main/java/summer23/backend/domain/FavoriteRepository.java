eb73b5da44584c3c0dc0076e9ac35579f34cc4c9
elinarou <elina_rouvinen@outlook.com>
2023-07-11 14:46:28 +0300

    Added repository searches

--- backend/src/main/java/summer23/backend/domain/FavoriteRepository.java
+++ backend/src/main/java/summer23/backend/domain/FavoriteRepository.java
@@ -8,4 +8,9 @@ public interface FavoriteRepository extends CrudRepository<Favorite, Long> {
     // Find by customer id
 	List<Favorite> findByCustomerId(Long id);
 
+    // Find by customer username
+	List<Favorite> findByCustomerUsername(String username);
+
+    // Find by note id
+	List<Favorite> findByNoteId(Long id);
 }
